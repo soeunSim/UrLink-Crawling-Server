3fefca1d8aa30b21acab610c2ab83f1a4d190dee
seonghoon <shee5032@naver.com>
2025-01-20 23:35:29 +0900

    chore: eslint 및 prettier 설정

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  { ignores: ["dist"] },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    settings: {},
+    plugins: {},
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+
+];
