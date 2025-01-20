cbce4dac277ac3f088257deb42b3a4cda503679b
seonghoon <shee5032@naver.com>
2025-01-20 23:57:03 +0900

    chore: prettier에 trivago plugin 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from "@eslint/js";
-import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
   { ignores: ["dist"] },
