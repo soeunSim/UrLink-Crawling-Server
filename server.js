a53fd91d3c1f53fe3f4e110f3641392ffa3ec3c2
seonghoon <shee5032@naver.com>
2025-01-22 15:49:40 +0900

    feat: cors 설정 추가

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const crawlRoute = require("./routes/crawlRoute.js");
 const app = express();
 const port = process.env.port || 3000;
 
 app.use("/crawl", crawlRoute);
+
+app.use(cors());
 
 app.listen(port);
