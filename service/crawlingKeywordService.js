8f8c08d5fe48e9de19b169af65fc25639fb7e98b
soeunSim <629943@naver.com>
2025-01-23 15:54:48 +0900

    feat: URL 크롤링 결과 에러처리

--- service/crawlingKeywordService.js
+++ service/crawlingKeywordService.js
@@ -1,5 +1,15 @@
 const puppeteer = require("puppeteer");
 
+function isCheckTrueThisUrl(url) {
+  /* eslint-disable */
+  const urlRegex = /^(http|https):\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  if (urlRegex.test(url)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 const getCrawlingKeyword = async (req, res) => {
   const decodedLink = decodeURIComponent(req.params.url);
   const browser = await puppeteer.launch({ headless: true });
@@ -12,14 +22,26 @@ const getCrawlingKeyword = async (req, res) => {
     let innerText = await page.evaluate(() => document.body.innerText);
     const hasKeyword = innerText.includes(req.query.keyword);
 
-    return res.status(200).json({
-      urlLink: req.params.url,
-      keyword: req.query.keyword,
-      hasKeyword: hasKeyword,
-      urlText: innerText,
-    });
+    if (hasKeyword === true) {
+      return res.status(200).json({
+        urlLink: req.params.url,
+        keyword: req.query.keyword,
+        hasKeyword: hasKeyword,
+        urlText: innerText,
+      });
+    } else if (hasKeyword === false) {
+      return res.status(200).send({ message: `[This keyword does not exist]` });
+    }
   } catch (error) {
-    return res.status(500).send({ message: `[ServerError occured] ${error}` });
+    if (isCheckTrueThisUrl(decodedLink) === false) {
+      return res
+        .status(400)
+        .send({ message: `[Invalid Characters in HTTP request]  ${error}` });
+    } else {
+      return res
+        .status(500)
+        .send({ message: `[ServerError occured] ${error}` });
+    }
   } finally {
     await browser.close();
   }
