f40b4da48a1b7310488931b5a2ba1ef63a3f9bd6
soeunSim <629943@naver.com>
2025-01-24 00:15:19 +0900

    feat: URL 크롤링 iframe 대처 및 에러처리

--- service/crawlingKeywordService.js
+++ service/crawlingKeywordService.js
@@ -11,13 +11,14 @@ function isCheckTrueThisUrl(url) {
 }
 
 const getCrawlingKeyword = async (req, res) => {
-  const decodedLink = decodeURIComponent(req.params.url);
+  const decodedUrl = decodeURIComponent(req.params.url);
   const browser = await puppeteer.launch({ headless: true });
+  const TIMEOUT = 20000;
 
   try {
     const page = await browser.newPage();
 
-    await page.goto(decodedLink);
+    await page.goto(decodedUrl);
 
     let innerText = await page.evaluate(() => document.body.innerText);
     const hasKeyword = innerText.includes(req.query.keyword);
@@ -29,11 +30,44 @@ const getCrawlingKeyword = async (req, res) => {
         hasKeyword: hasKeyword,
         urlText: innerText,
       });
+    } else if (innerText === "") {
+      await page.waitForSelector("iframe", { timeout: TIMEOUT });
+      const iframeUrl = await page.evaluate(
+        () => document.querySelector("iframe").src
+      );
+
+      await page.goto(iframeUrl);
+
+      const hasiframeUrlOfNaver = iframeUrl.startsWith(
+        "https://blog.naver.com"
+      );
+      let iframeInnerText = await page.evaluate(() => document.body.innerText);
+      const hasKeywordOfIframe = iframeInnerText.includes(req.query.keyword);
+
+      if (!iframeUrl || !hasiframeUrlOfNaver) {
+        throw new Error(`[Invalid iframe URL]`);
+      }
+      if (hasKeywordOfIframe === true) {
+        return res.status(200).json({
+          urlLink: req.params.url,
+          keyword: req.query.keyword,
+          hasKeyword: hasKeywordOfIframe,
+          urlText: iframeInnerText,
+        });
+      } else if (hasKeywordOfIframe === false) {
+        return res
+          .status(200)
+          .send({ message: `[This keyword does not exist]` });
+      } else {
+        throw new Error(
+          `[The requested page does not exist or a problem occurred]`
+        );
+      }
     } else if (hasKeyword === false) {
       return res.status(200).send({ message: `[This keyword does not exist]` });
     }
   } catch (error) {
-    if (isCheckTrueThisUrl(decodedLink) === false) {
+    if (isCheckTrueThisUrl(decodedUrl) === false) {
       return res
         .status(400)
         .send({ message: `[Invalid Characters in HTTP request]  ${error}` });
