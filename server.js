8135da937c7357dcdf2170a082aa64642f8721d2
soeunSim <629943@naver.com>
2025-01-24 14:50:06 +0900

    refactor: 전체 코드 컨벤션 정리

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const crawlRoute = require("./routes/crawlRoute.js");
 const app = express();
 const port = process.env.port || 3000;
 
-app.use("/crawl", crawlRoute);
-
 app.use(cors());
+
+app.use("/crawl", crawlRoute);
 
 app.listen(port);
