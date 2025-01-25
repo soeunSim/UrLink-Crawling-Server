86217b7111d4a46f0dbf9e3ff0d1d9b078c9c410
sesim <629943@naver.com>
2025-01-25 22:03:13 +0900

    feat: URL 크롤링 결과 에러처리 및 iframe 대처 (#5)
    * chore: 불필요한 파일 삭제
    
    * feat: URL 크롤링 로직 구현 및 API 개발
    
    * refactor: eslint.config.js 최상단 모듈 관련 내용 컨벤션 수정
    
    * refactor: app.listen의 console.log 제거
    
    * refactor: 화살표 함수 컨벤션 수정
    
    * refactor: url 크롤링 할 때 가져오는 데이터를 textContent에서 innerText로 변경
    
    * refactor: 검색한 키워드가 포함되어 있지 않은 url일 때, 특별한 메시지를 표시하지 않게 수정
    
    * feat: cors 설정 추가
    
    * refactor: crawlingKeywordService파일과 crawlingTitleService파일에 각각 불필요한 기능 관련 코드 제거
    
    * feat: URL 크롤링 결과 에러처리
    
    * feat: URL 크롤링 iframe 대처 및 에러처리
    
    * refactor: 전체 코드 컨벤션 정리
    
    * refactor: 코드 컨벤션 정리
    
    * refactor: 함수형 컨벤션 정리
    
    ---------
    
    Co-authored-by: seonghoon <shee5032@naver.com>

--- service/crawlingKeywordService.js
+++ service/crawlingKeywordService.js
@@ -1,25 +1,82 @@
 const puppeteer = require("puppeteer");
 
+const isCheckTrueThisUrl = (url) => {
+  /* eslint-disable */
+  const urlRegex = /^(http|https):\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  if (urlRegex.test(url)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const getCrawlingKeyword = async (req, res) => {
-  const decodedLink = decodeURIComponent(req.params.url);
+  const decodedUrl = decodeURIComponent(req.params.url);
   const browser = await puppeteer.launch({ headless: true });
+  const TIMEOUT = 20000;
 
   try {
     const page = await browser.newPage();
 
-    await page.goto(decodedLink);
+    await page.goto(decodedUrl);
 
     let innerText = await page.evaluate(() => document.body.innerText);
     const hasKeyword = innerText.includes(req.query.keyword);
 
-    return res.status(200).json({
-      urlLink: req.params.url,
-      keyword: req.query.keyword,
-      hasKeyword: hasKeyword,
-      urlText: innerText,
-    });
+    if (hasKeyword) {
+      return res.status(200).json({
+        url: req.params.url,
+        urlLink: req.params.url,
+        eyword,
+        hasKeyword: hasKeyword,
+        urlText: innerText,
+      });
+    } else if (innerText === "") {
+      await page.waitForSelector("iframe", { timeout: TIMEOUT });
+      const iframeUrl = await page.evaluate(
+        () => document.querySelector("iframe").src
+      );
+
+      await page.goto(iframeUrl);
+
+      const hasiframeUrlOfNaver = iframeUrl.startsWith(
+        "https://blog.naver.com"
+      );
+      let iframeInnerText = await page.evaluate(() => document.body.innerText);
+      const hasKeywordOfIframe = iframeInnerText.includes(req.query.keyword);
+
+      if (!iframeUrl || !hasiframeUrlOfNaver) {
+        throw new Error(`[Invalid iframe URL]`);
+      }
+      if (hasKeywordOfIframe) {
+        return res.status(200).json({
+          urlLink: req.params.url,
+          keyword: req.query.keyword,
+          hasKeyword: hasKeywordOfIframe,
+          urlText: iframeInnerText,
+        });
+      } else if (!hasKeywordOfIframe) {
+        return res
+          .status(200)
+          .send({ message: `[This keyword does not exist]` });
+      } else {
+        throw new Error(
+          `[The requested page does not exist or a problem occurred]`
+        );
+      }
+    } else if (!hasKeyword) {
+      return res.status(200).send({ message: `[This keyword does not exist]` });
+    }
   } catch (error) {
-    return res.status(500).send({ message: `[ServerError occured] ${error}` });
+    if (isCheckTrueThisUrl(decodedUrl) === false) {
+      return res
+        .status(400)
+        .send({ message: `[Invalid Characters in HTTP request]  ${error}` });
+    } else {
+      return res
+        .status(500)
+        .send({ message: `[ServerError occured] ${error}` });
+    }
   } finally {
     await browser.close();
   }
